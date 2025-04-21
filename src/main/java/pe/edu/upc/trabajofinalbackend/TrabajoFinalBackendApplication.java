83056740f30d1877a28cc4e3d39309427a8f5493
Sebastian <u202015200@upc.edu.pe>
2025-04-20 19:14:55 -0500

    Creación del repositorio

---
+++ src/main/java/pe/edu/upc/trabajofinalbackend/TrabajoFinalBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.trabajofinalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoFinalBackendApplication.class, args);
+    }
+
+}
